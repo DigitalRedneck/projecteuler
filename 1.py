87658d0ac0cdef658fab6a7060a266edef2805d6
NorthRedneck <dkorelskiy@gmail.com>
2019-10-14 16:17:51 +0300

    Solved Euler problem #1

--- 1.py
+++ 1.py
@@ -1,8 +1,25 @@
+print('the Euler Project problem #1 solving')
 
-hello_string = input("What is your name?")
+current_number = 1
+correct_numbers_list = []
+answer = 0
 
-print("Hello wold, and hi to " + hello_string + "!!!")
+while current_number < 1000:                                
+    if (current_number % 3) == 0:                       #checking this number multiplies of 3
+      
+        correct_numbers_list.append(current_number)
 
-#prevent to close terminal
-#change for git
+    elif (current_number % 5) == 0:                     #checking this number multiplies of 5
+      
+        correct_numbers_list.append(current_number)
+    
+    current_number += 1
+else:
+    pass
+    #print(correct_numbers_list)                         #printing list of numbers mutiplies of 3 and 5
+for counter in correct_numbers_list:
+    answer += counter                                   #finding sum of list of numbers
+print("Answer is " + str(answer))
+    
+
 input()
